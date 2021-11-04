7bff6011456253af23437aa7da30304cde4d973d
cristian torres <ctorresolarte@gmail.com>
2021-11-03 19:55:56 -0500

    Initial commit

---
+++ app/src/main/java/com/ingcrisman/proyectoactivites/MainActivity.java
@@ -0,0 +1,71 @@
+package com.ingcrisman.proyectoactivites;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
+        Log.i("Information", "onCreate");
+    }
+
+    public void goToActivitySecond(View view){
+        Intent newIntent = new Intent(this, SecondActivity.class);
+        newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        newIntent.putExtra("msg", "Hola programador");
+        newIntent.putExtra("year", "2021");
+        startActivity(newIntent);
+    }
+
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+        Log.i("Information", "onDestroy");
+    }
+
+
+
+
+
+
+}
